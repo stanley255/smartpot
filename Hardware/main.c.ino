e23d1aaa33af2d7b214c7bc6fb93e0d7738d629d
adik6555 <adik6555@gmail.com>
2018-10-10 18:59:32 +0200

    Sprehladnenie 1.verzie kodu

--- Hardware/main.c.ino
+++ Hardware/main.c.ino
@@ -2,27 +2,30 @@
 #include <ESP8266HTTPClient.h>
 #include <ESP8266Wifi.h>
 #include <string.h>
-/*SSID PART*/
-#define SSID "xiaomi"
-#define PSWD "12345678"
+/*DEFINITIONS PART*/
+#define SSID "xiaomi"                                                                    /*change this to your WiFi SSID*/
+#define PSWD "12345678"                                                                  /*change this to your WiFi password*/
 #define sensorPin 0
+#define ID 1
 /*HTTP PART*/
 HTTPClient http;
 
-char strToSend[200];
-char const saveSensorDataURL[100]="http://robocode.sk/smartpot/php/saveSensorData.php?";
-char GETPart[100];
+/*
+ * Global Variables - these are defined to lessen 
+ * the number of parameters passed to functions
+*/
+char const saveSensorDataURL[100]="http://robocode.sk/smartpot/php/saveSensorData.php?"; /*The Adress of server you want to send the data to*/
+float tmp;                                                                               /*Temperature measurements go here*/
+float hmd;                                                                               /*Humidity measurements go here*/
 
-int id =1;
-float tmp;
-float hmd;
 
 void setup() { 
-  Serial.begin(115200);
-  WiFi.begin(SSID, PSWD);
+  Serial.begin(115200);                                                                  /*Initializes serial connection - for debug purposes*/
+  WiFi.begin(SSID, PSWD);                                                                /*Initializes WiFi connection*/
 }
 
-float readTemp(){
+
+float readVals(){                                                                        /*Function handling the reading of temperature(and humidity? :) ) from the sensors*/
   int reading=analogRead(sensorPin);
   float voltage = reading * 3.3; 
   voltage /= 1024.0; 
@@ -30,28 +33,31 @@ float readTemp(){
   return temperature;
 }
 
-char *strHandling(char *saveSensorDataURL, char* GETPart, char* strToSend){
-  memset(GETPart,0,strlen(GETPart));
-  memset(strToSend,0,strlen(strToSend));
-  tmp=random(1,10);
-  sprintf(GETPart,"id=%d&temp=%lf",id,tmp);
-  strcat(strToSend,saveSensorDataURL);
-  strcat(strToSend,GETPart);
-  Serial.println(strToSend);
-  return (strToSend);
-}
- 
-void loop() {
-  while (WiFi.status()!=WL_CONNECTED){
-    Serial.println("connecting");
-    delay(500);
-  }
-
+void createString(char *strToSend, float tmp, float hmd){                               /*Function creating the string which is to be sent by httpGetRequest*/
+  char *valString=(char*) malloc (50);
   
+  sprintf(valString,"id=%d&temp=%lf",ID,tmp);
+  strcat(strToSend,saveSensorDataURL);
+  strcat(strToSend,valString);  
+  free (valString);
+}
+
+void httpGetRequest(float tmp, float hmd){                                              /*Function sending http get request to a server*/
+  char *strToSend=(char*) malloc (200);
+  createString(strToSend,tmp,hmd);  
+  
+  Serial.println(strToSend);
+
   http.begin(strToSend);
   int httpCode=http.GET();
   http.end();
-  delay(10000);
-  
+  free(strToSend);
+}
+ 
+void loop() {
+  while (WiFi.status()!=WL_CONNECTED){                                                  /*Connecting to the WiFi network and checking connection status*/
+    Serial.println("connecting");
+    delay(500);
+  }
   
 }
