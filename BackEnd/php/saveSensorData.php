89378301c5898b1dbb885fefbebb0914e1a76fca
Stanislav Pekarovic <spekarovic@gmail.com>
2018-10-11 21:28:42 +0200

    Humidity Added

--- BackEnd/php/saveSensorData.php
+++ BackEnd/php/saveSensorData.php
@@ -1,11 +1,20 @@
 <?php
   require '../sql/config.php';
+  header('Content-Type: application/json');
+  // Retrieve collected data from request
   $xProductId   = $_GET["id"];
-  $xTemperature = str_replace(',','.',$_GET["temp"]);
-  $response["id"] = $xProductId;
-  $response["temp"] = $xTemperature;
-  if ($stmt = mysqli_prepare($con, "INSERT INTO COLLECTED_DATA(fk_product_id, temperature) VALUES(?,?)")){
-    if (mysqli_stmt_bind_param($stmt,"id",$xProductId,$xTemperature)){
+  $xTemperature = $_GET["temp"];
+  $xHumidity    = $_GET["hum"];
+  // Check data
+  if (is_null($xProductId) or is_null($xTemperature) or is_null($xHumidity)){
+    exit;
+  }
+  // Correct data if necessary
+  $xTemperature = str_replace(',','.',$xTemperature);
+  $xHumidity    = str_replace(',','.',$xHumidity);
+  // Insert values into DataBase
+  if ($stmt = mysqli_prepare($con, "INSERT INTO COLLECTED_DATA(fk_product_id, temperature, humidity) VALUES(?,?,?)")){
+    if (mysqli_stmt_bind_param($stmt,"idd",$xProductId,$xTemperature,$xHumidity)){
       if (mysqli_stmt_execute($stmt)){
         mysqli_stmt_close($stmt);
         $response["action"] = 1;
