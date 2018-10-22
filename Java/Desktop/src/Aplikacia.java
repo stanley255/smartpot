a3036a3cbdfca46080508231cced63fb698e5279
Zuzka <zuzanka.lehutova@gmail.com>
2018-10-22 21:46:15 +0200

    pridané okno, kde sa zobrazí response

--- Java/Desktop/src/Aplikacia.java
+++ Java/Desktop/src/Aplikacia.java
@@ -2,6 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
@@ -9,9 +12,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 
-public class Aplikacia {
+public class Aplikacia extends JFrame {
 
 	public static void main(String[] args) throws ClientProtocolException, IOException {
+		
+		
 		
 		String url = "http://robocode.sk/smartpot/php/getLatestSensorData.php?id=1";
 
@@ -34,6 +39,18 @@ public class Aplikacia {
 		}
 
 		System.out.println(result);
+		
+		
+		JFrame okno = new JFrame("SmartPot");
+		
+		
+		JTextArea txaText = new JTextArea();
+		txaText.append(result.toString());
+		
+		okno.add(txaText);
+		okno.setSize(1000, 300);
+		okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		okno.setVisible(true);
 		
 		request.releaseConnection();
 	}
