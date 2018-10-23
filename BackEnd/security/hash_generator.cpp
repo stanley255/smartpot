ce26b9bd54ce8ad67f293ac0d0deb74254e3257b
Stanislav Pekarovic <spekarovic@gmail.com>
2018-10-23 14:31:51 +0200

    Hash Generator Bug Fix

--- BackEnd/security/hash_generator.cpp
+++ BackEnd/security/hash_generator.cpp
@@ -4,73 +4,77 @@
 
 using namespace std;
 
-class Hash{
-  public:
-    string generated_hash;
-    int hashLength, countNumbers, countUpperLetters, countLowerLetters, countSpecialChars;
-    int countTypesOfParams = 4; // Konstanta urcujuca pocet typov argmunetov
-    
-    int getRandomNumber(){
-      return rand()%(10);
-    }
-    char getRandomUppLett(){
-      return rand()%(25)+65; 
-    }
-    char getRandomLowLett(){
-      return rand()%(25)+97; 
-    }
-    char getRandomSpecChar(){
-      return rand()%(14)+34;
-    }
-  public:
-    Hash(int countNums, int countUppLett, int countLowLett, int countSpecChars){
-      countNumbers      = countNums;
-      countUpperLetters = countUppLett;
-      countLowerLetters = countLowLett;
-      countSpecialChars = countSpecChars;
-      hashLength = countNums + countUppLett + countLowLett + countSpecChars;
-    }
-    string getHash(){
-      int initialLength = 0;
-      int missCounter = 0;
-      while (initialLength != hashLength){
-        int randomType = rand()%countTypesOfParams;
-        switch (randomType){
-          case 0:
-            if (countNumbers){
-              generated_hash += this->getRandomNumber();
-              countNumbers--;
-              initialLength++;
-            }             
-          case 1:
-            if (countUpperLetters){
-              generated_hash += this->getRandomUppLett();
-              countUpperLetters--;
-              initialLength++;
-            }            
-          case 2:
-            if (countLowerLetters){
-              generated_hash += this->getRandomLowLett();
-              countLowerLetters--;
-              initialLength++;
-            }            
-          case 3:
-            if (countSpecialChars){
-              generated_hash += this->getRandomSpecChar();
-              countSpecialChars--;
-              initialLength++;
-            }        
-          default:
-            missCounter++;
-        }
-      } 
-      return generated_hash;      
-    }
+class Hash {
+public:
+	string generated_hash;
+	int hashLength, countNumbers, countUpperLetters, countLowerLetters, countSpecialChars;
+	int countTypesOfParams = 4; // Konstanta urcujuca pocet typov argmunetov
+
+	int getRandomNumber() {
+		return rand() % (10);
+	}
+	char getRandomUppLett() {
+		return rand() % (25) + 65;
+	}
+	char getRandomLowLett() {
+		return rand() % (25) + 97;
+	}
+	char getRandomSpecChar() {
+		return rand() % (14) + 34;
+	}
+public:
+	Hash(int countNums, int countUppLett, int countLowLett, int countSpecChars) {
+		countNumbers = countNums;
+		countUpperLetters = countUppLett;
+		countLowerLetters = countLowLett;
+		countSpecialChars = countSpecChars;
+		hashLength = countNums + countUppLett + countLowLett + countSpecChars;
+	}
+	string getHash() {
+		int initialLength = 0;
+		int missCounter = 0;
+		while (initialLength != hashLength) {
+			int randomType = rand() % countTypesOfParams;
+			switch (randomType) {
+			case 0:
+				if (countNumbers) {
+					generated_hash += to_string(this->getRandomNumber());
+					countNumbers--;
+					initialLength++;
+				}
+				break;
+			case 1:
+				if (countUpperLetters) {
+					generated_hash += this->getRandomUppLett();
+					countUpperLetters--;
+					initialLength++;
+				}
+				break;
+			case 2:
+				if (countLowerLetters) {
+					generated_hash += this->getRandomLowLett();
+					countLowerLetters--;
+					initialLength++;
+				}
+				break;
+			case 3:
+				if (countSpecialChars) {
+					generated_hash += this->getRandomSpecChar();
+					countSpecialChars--;
+					initialLength++;
+				}
+				break;
+			default:
+				missCounter++;
+			}
+		}
+		return generated_hash;
+	}
 };
 
-int main(){
-  srand(time(NULL));
-  Hash my_hash = Hash(12,12,12,12);
-  cout << my_hash.getHash() << endl;
-  return 0;
+int main() {
+	srand(time(NULL));
+	Hash my_hash = Hash(12, 12, 12, 12);
+	cout << my_hash.getHash() << endl;
+	return 0;
 }
