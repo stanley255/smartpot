50b34e0aecde6f8b36ba1e7bec9a6859620928d6
adik6555 <adik6555@gmail.com>
2018-11-08 23:13:00 +0100

    Restructured the code to fit c++ standards.

--- Hardware/main.c/main.c.ino
+++ Hardware/main.c/main.c.ino
@@ -1,27 +1,32 @@
-/*LIBRARIES REQUIRED*/
+/*************************************************************LIBRARIES REQUIRED*************************************************************/
+
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 #include <ESP8266Wifi.h>
 #include <string.h>
 #include <ESP8266WebServer.h>
 //#include <sha256.h>
-/*DEFINITIONS PART*/
-#define SSID "xiaomi"                                                                    /*change this to your WiFi SSID*/
-#define PSWD "12345678"                                                                  /*change this to your WiFi password*/
-#define sensorPin 0
-#define ID 1
-/*HTTP PART*/
-HTTPClient http;
 
-/*
- * Global Variables - these are defined to lessen 
- * the number of parameters passed to functions
-*/
-char const saveSensorDataURL[100]="http://robocode.sk/smartpot/php/saveSensorData.php?"; /*The Adress of server you want to send the data to*/
-char saltAdress[100]="http://robocode.sk/smartpot/php/getSecurityToken.php";
+/**************************************************************DEFINITIONS PART**************************************************************/
+/*******************************************Change these in correspondence to your implementation.*******************************************/
+
+#define SSID "xiaomi"                                                                   /*YOUR WIFI SSID*/
+#define PSWD "12345678"                                                                 /*YOUR WIFI PASSWORD*/
+#define tempPin 0                                                                       /*Pin where the temp sensor is connected to*/ 
+#define ID 1                                                                            /*ID No. of the board*/
+String const saveSensorDataURL="http://robocode.sk/smartpot/php/saveSensorData.php?";   /*The Adress of server you want to send the data to*/
+String saltAdress="http://robocode.sk/smartpot/php/getSecurityToken.php";               /*The Adress to get the security key from*/
+
+/**************************************************************GLOBAL VARIABLES***************************************************************/
+/**********************************These are defined to lessen the number of parameters passed to functions.**********************************/
+/**********************************Very likely to be replaced in more elegant fashion in the future updates.**********************************/
+
 float tmp;                                                                               /*Temperature measurements go here*/
-float hmd;                                                                               /*Humidity measurements go here*/
+float hum;                                                                               /*Humidity measurements go here*/
+HTTPClient http;
 DynamicJsonBuffer jsonBuffer(200);
+
+/***************************************************************THE MAIN CODE*****************************************************************/
 
 void setup() { 
   Serial.begin(115200);                                                                  /*Initializes serial connection - for debug purposes*/
@@ -30,19 +35,17 @@ void setup() {
 
 
 float readVals(){                                                                        /*Function handling the reading of temperature(and humidity? ) from the sensors*/
-  int reading=analogRead(sensorPin);
+  int reading=analogRead(tempPin);
   float voltage = reading * 3.3; 
   voltage /= 1024.0; 
-  tmp = (voltage - 0.5) * 100;
+  temp = (voltage - 0.5) * 100;
 }
 
-void createString(char *strToSend, float tmp, float hmd){                               /*Function creating the string which is to be sent by httpGetRequest*/
-  char *valString=(char*) malloc (70);
+String createString(String strToSend, float tmp, float hmd){                             /*Function creating the string which is to be sent by httpGetRequest*/
+  String valString="id="+String(ID)+"&tmp="+String(tmp)+"&hum="+String(hmd);
+  strToSend=strToSend+saveSensorDataURL+valString;
+  return strToSend;
   
-  sprintf(valString,"id=%d&temp=%lf&hum=%lf",ID,tmp,hmd);
-  strcat(strToSend,saveSensorDataURL);
-  strcat(strToSend,valString);  
-  free (valString);
 }
 
 String getSecurityKey(){
@@ -67,16 +70,14 @@ String getSecurityKey(){
 //}
 
 void httpGetRequest(float tmp, float hmd){                                              /*Function sending http get request to a server*/
-  char *strToSend=(char*) malloc (200);
-  *strToSend='\0';
-  createString(strToSend,tmp,hmd);  
+  String strToSend;
+  strToSend=createString(strToSend,tmp,hmd);  
   
   Serial.println(strToSend);
 
   http.begin(strToSend);
   int httpCode=http.GET();
   http.end();
-  free(strToSend);
 }
  
 void loop() {
@@ -84,6 +85,7 @@ void loop() {
     Serial.println("connecting");
     delay(500);
   }
+  //httpGetRequest(tmp,hum);
   getSecurityKey();
   delay(5000);
   
