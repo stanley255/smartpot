83aeba3644de7d7d03624034af6df75544f622ef
Matej Friedel <m.friedel99@gmail.com>
2019-06-11 16:17:53 +0200

    Minor changes.

--- Java/SmartPot/src/main/java/Main.java
+++ Java/SmartPot/src/main/java/Main.java
@@ -1,5 +1,9 @@
+import GUI.SmartpotFrame;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello SmartPot!");
+
+        SmartpotFrame smartpotFrame = new SmartpotFrame();
     }
 }
